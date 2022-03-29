ffff97ebfc63aa697dfe9f0c61a88da2f4c32ae0
memkeytm <memkey@gmail.com>
2022-03-30 07:38:38 +0800

    Update test.ts
    更新

--- src/test.ts
+++ src/test.ts
@@ -6,33 +6,73 @@ export interface INode {
 }
 
 
-fabric.Image.filters.Redify = fabric.util.createClass(fabric.Image.filters.BaseFilter, {
-    type: 'Redify',
-
-    /**
-     * Fragment source for the redify program
-     */
-    fragmentSource: 'precision highp float;\n' +
-      'uniform sampler2D uTexture;\n' +
-      'varying vec2 vTexCoord;\n' +
-      'void main() {\n' +
-        'vec4 color = texture2D(uTexture, vTexCoord);\n' +
-        'color.g = 0.0;\n' +
-        'color.b = 0.0;\n' +
-        'gl_FragColor = color;\n' +
-      '}',
+fabric.Myfilters.base = fabric.util.createClass(fabric.Object, {
+    type: 'AnchorPointMove',
   
-    applyTo2d: function(options) {
-      var imageData = options.imageData,
-          data = imageData.data, i, len = data.length;
-  
-      for (i = 0; i < len; i += 4) {
-        data[i + 1] = 0;
-        data[i + 2] = 0;
-      }
+    apply: function(options: any) {
+        let startValue = options['startValue'],
+        endValue = options['endValue'],
+        duration = options['duration'],
+        currentTime = options['currentTime'],
+        easing = options['easing'],
+        value = easing(currentTime, startValue, endValue, duration);
+        this.filter(value);
+    },
 
+    filter: function(value: any){
+        throw('no implemnt');
     }
-  });
+});
+
+fabric.Myfilters.AnchorPointMove = fabric.util.createClass(fabric.Myfilters.base, {
+    filter: function(value){
+        this.set(this.key, value)
+        //this.container.set('anchorPoint', value);    
+    }
+})
+
+fabric.Myfilters.opacity = fabric.util.createClass(fabric.Myfilters.base, {
+    filter: function(value){
+        this.container.set('opacity', value);    
+    }
+})
+
+fabric.Myfilters.clipPath = fabric.util.createClass(fabric.Myfilters.base, {
+    filter: function(value){
+        var clipPath = new fabric.Circle({
+            radius: value,
+            top: this.top,//外面传入
+            left: this.left//外面传入
+          });        
+        this.container.clipPath = clipPath;
+    }
+})
+
+
+fabric.Container = fabric.util.createClass(fabric.Object, {
+        initialize: function(obj, options) {
+          this.callSuper('initialize', options);
+          this.innerObj = obj;
+        },
+
+        _render: function(ctx) {
+          if (this.loaded) {
+            ctx.fillStyle = '#fff';
+            ctx.fillRect(
+              -(this.width / 2) - this.H_PADDING,
+              -(this.height / 2) - this.H_PADDING,
+              this.width + this.H_PADDING * 2,
+              this.height + this.V_PADDING * 2);
+            //主要用来做左右，上下的变换
+            //this.anchorPoint.x
+            //this.anchorPoint.y
+            //修改原点
+            //ctx.translate(0,canvasHeight);
+            this.innerObj.render(ctx);
+            //ctx.drawImage(this.image, -this.width / 2, -this.height / 2);
+          }
+        }
+});
 
 class Track{
     id : string;
@@ -73,11 +113,10 @@ class sourceCache extends Map<string, any>{
 }
 
 class fabricTimeLine extends timeLine{
-    load(data: any): void {
+    load(data: any, clear: boolean=false): void {
         let videoTrack:Track = new Track();
-        //读取工程数据        
+        //读取工程数据,同步对比数据        
         this.videoTracks.push(videoTrack);
-
         let audioTrack:Track = new Track();
         this.audioTracks.push(audioTrack);
     }
@@ -161,6 +200,7 @@ abstract class Node implements INode {
     private FCanvas: any;
     private fabricObj: any;
     protected conf: any;
+    private animates: any;
     protected needBuffer: boolean;
 
     constructor(conf: any, fabric: any) {
@@ -178,9 +218,10 @@ abstract class Node implements INode {
         }
 
         if (time >= this.conf['from'] && time < this.conf['to']) {
-            if (this.isFabricObjHasAdd()){
+            if (!this.isFabricObjHasAdd()){
                 this.FCanvas.add(this.fabricObj);
             }
+            this.applyAnimates(time);
             if (this.needBuffer){
                 //通知需要缓冲
             }
@@ -193,12 +234,13 @@ abstract class Node implements INode {
         //更新坐标，大小，旋转角度等属性
         //this.fabricObj
         //挂载特效滤镜
+
     }
 
-    addFilters():void{
-        
-        this.fabricObj.filters.push()
-        this.fabricObj.applyFilters();
+    applyAnimates(time):void{        
+        this.animates.forEach(animate => {
+            animate.apply(time);
+        });
     }
 
     isFabricObjHasAdd():boolean{
@@ -208,16 +250,10 @@ abstract class Node implements INode {
     abstract createFabricObj(): any;
 }
 
-class ImageNode extends Node {
-      
+class ImageNode extends Node {      
     createFabricObj(): any { 
         let source = createHtml5Element(this.conf['id'], this.conf['url'], HTMLImageElement);                  
-        return new fabric.Image(source, {
-            left: this.conf['left'],
-            top: this.conf['top'],
-            angle: this.conf['angle'],
-            opacity: this.conf['opacity']
-        })
+        return fabric.Container(new fabric.Image(source))
     }
 }
 
@@ -233,26 +269,28 @@ class shapeNode extends Node{
                 });
             })
         };
+        //
+        //return fabric.Container(new fabric.Image(source));
     }
 }
 
 class gifNode extends Node{
     createFabricObj() {
-        let source = null;
-        return new fabric.Image(source, {
-            left: this.conf['left'],
-            top: this.conf['top'],
-            angle: this.conf['angle'],
-            opacity: this.conf['opacity'],
-
-        })        
+        let source = null;//canvas
+        //构建gif解码器
+        return fabric.Container(new fabric.Image(source));        
     }
 }
 
 class videoNode extends ImageNode{
+    playbackRate : number;//后续倍速播放在这个基础上乘以倍速
     createFabricObj(): any {   
         let source1 = createHtml5Element(this.conf['id'], this.conf['url'], HTMLImageElement);  
         let source :HTMLVideoElement = null;
+        //获取文件原始帧率
+        let videoOrgFramerate = 30;
+        this.playbackRate = videoOrgFramerate / 25
+        source.playbackRate = this.playbackRate;
         let self = this;
         source.addEventListener('timeupdate', function(){
             if ((source.currentTime < self.conf['trimStart']) || source.currentTime > self.conf['trimEnd']){
@@ -260,12 +298,7 @@ class videoNode extends ImageNode{
             }
         });
 
-        return new fabric.Image(source, {
-            left: this.conf['left'],
-            top: this.conf['top'],
-            angle: this.conf['angle'],
-            opacity: this.conf['opacity'],
-        })
+        return fabric.Container(new fabric.Image(source));
     }
 
     public render(time: number): void {
@@ -280,10 +313,13 @@ class videoNode extends ImageNode{
 }
 
 class audioNode extends Node {
-
+    playbackRate : number;//后续倍速播放在这个基础上乘以倍速
     createFabricObj(): any {
         let source1 = createHtml5Element(this.conf['id'], this.conf['url'], HTMLAudioElement);  
         let source :HTMLVideoElement = null;
+        //获取文件原始帧率
+        let videoOrgFramerate = 30;
+        this.playbackRate = videoOrgFramerate / 25        
         let self = this;        
         source.addEventListener('timeupdate', function(){
             if ((source.currentTime < self.conf['trimStart']) || source.currentTime > self.conf['trimEnd']){
